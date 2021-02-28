9386942cd603ab43882118605b36b0ccbe6f44f4
Andrew Kim <20strategy@gmail.com>
2021-02-27 17:53:42 -0800

    404 page added

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1 style="text-align:center">404- page not found</h1>
+<div style="text-align: center; margin-top:100px;">
+    <g-image src="~/assets/error.png"></g-image>
+    <h1> Wrong Page</h1>
+    <h1> Take me to the Home Page</h1>
+    <g-link exact to="/"><button style="font-size: 30px; color:red;">😀 Click Here ☕️  </button></g-link>
+</div>
 </template>
